4df40af08e9173e2e3885b0e8449146f8fc18a49
The Color Blurple <turtlenerd603@gmail.com>
2023-11-13 00:12:26 -0500

    typescript test

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ This fork was created cause the old site and repo is a mess, and i don't want an
 
 Feel free to fork and add your own features, but there is no guarantee i will merge them. Please submit them as PRs, not full commits.
 
+I should also mention that you need to keep both the [Typescript](https://github.com/microsoft/TypeScript) (`.ts`) and compiled Typescript (`.js`) should be committed to this repo. All TS Map files are ignored as they're only for debugging and IDE support.
+
+To get started, run `npm start`, wait for all the packages to download, and you should be given the Typescript Watcher in that console. Keep it running while you modify the files. Run `npm run` or whatever you have as a keybind for it to start the server. It should be located at [http://localhost:8080](http://localhost:8080)
+
 ## written plans
 
 Anything pertaining to an account system has been postponed. Considering i was going to use Microsoft OAuth for it and this being a majority cracked server, i need another plan. Keeping the Alias-IP system until further notice.
@@ -37,53 +41,3 @@ Anything pertaining to an account system has been postponed. Considering i was g
 - [ ] Make it as small and clean as possible
   - [x] Have it be fast and small enough to run on 256mb of ram so it can run on even the cheapest of servers (currently using [FL0](https://www.fl0.com/))
   - [ ] Small file footprint for more Wiki pages
-
-## structure
-
-### wiki
-
-The wiki uses a markdown file ran through a MD-HTML converter to add new pages, as it makes it much faster to write them. A tool is in the works that allows you to write pages automatically.
-
-here is a basic wiki page, using HTML format for now.
-
-```html
-<span class="tag tag-green">Location</span>
-<span class="tag tag-blue">English</span>
-<br><br>
-<span class="tag tag-blurple">DJzombiehunter</span>
-
-<h2>Placeholder Title</h2>
-<h3>Summary</h3>
-<p>
-    placholder
-</p>
-
-<h3>Residents</h3>
-<ul>
-    <li><a href="#alakazam6898">@Alakazam6898</a> (Owner / Main Builder)</li>
-</ul>
-
-<h3>Maps, Images, and References</h3>
-<!-- Use the button to hide/show the image, and hide it by default -->
-<button onclick="toggleElementVisibility('cublak-view_13/06/23')">Click to show image "cublak-view_13/06/23"</button>
-<img id="cublak-view_13/06/23" src="https://media.discordapp.net/attachments/1061516148325220455/1118033620024709120/image.png" alt="Image 1" style="display:none;">
-
-<ul>
-    <li><a href="#reference-id">reference-placholder</a></li>
-</ul>
-
-<h3>Trivia</h3>
-<li>
-    The name Cublak View is made of 3 parts; Cube > Cub, Lakshyapowerman > Lak, Mountain View > View. It was a name idea that everyone just stuck with.
-</li>
-<li>
-    This is one of the 4 original towns on the server; being Spawn City, Cublak View, Windswept Fang, and Nether Bay.
-</li>
-
-```
-
-Do not use any `<head>`,`<body>`, or `<html>` tags in the wiki page files, as it will break the site completely if that occurs. Any page that contains the `<meta>` tag will not display for security and technical reasons.
-
-### files
-
-if you need to add a file, like an image, video, or subtitles, add it to the [`assets`](./assets/) directory.
--- misc.js
+++ misc.js
@@ -5,12 +5,26 @@ Under GPL v3 license
 Most of this code isnt mine anyway, thanks stackoverflow
 
 */
-
-function hexToDecimal(hex) {
-    return parseInt(hex.replace("#", ""), 16)
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function checkIPAddressFunc(ip) {
+    const regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if (!regex.test(ip)) {
+        return "0:0:0:0";
+    }
+    return ip;
 }
-
-function genHexString(len) {
+function hexToDecimalFunc(hex) {
+    return parseInt(hex.replace("#", ""), 16);
+}
+function genHexStringFunc(len) {
     const hex = '0123456789ABCDEF';
     let output = '';
     for (let i = 0; i < len; ++i) {
@@ -18,46 +32,44 @@ function genHexString(len) {
     }
     return output;
 }
-
-function sleep(ms) {
+function sleepFunc(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-async function getBlockedIps(ip) {
-    try {
-        let response
-        let data
-
-        // try VPN ip list
-        response = await fetch('https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt');
-        data = await response.text();
-        data = data.split('\n');
-        for (let i = 0; i < data.length; i++) {
-            data[i] = data[i].split("/")[0];
+function getBlockedIpsFunc(ip) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let response;
+            let data;
+            // try VPN ip list
+            response = yield fetch('https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt');
+            data = yield response.text();
+            data = data.split('\n');
+            for (let i = 0; i < data.length; i++) {
+                data[i] = data[i].split("/")[0];
+            }
+            if (ip in data) {
+                return false; // false means unsafe
+            }
+            // try get-ip-intel
+            response = yield fetch(`https://www.getipintel.net/check.php?ip=${ip}&flags=m`); // https://www.getipintel.net/ open an issue to change the flags
+            data = yield response.text();
+            if (data === '1') {
+                return false; // false means unsafe
+            }
+            // if it passes both checks;
+            return true; // true means safe
         }
-        if (ip in data) {
-            return false // false means unsafe
+        catch (error) {
+            console.error(`An error has occurred ; ${error} ; this user has been excused from the ip blocker`);
+            return true; // you could cause an error to occur and get around the block, but its so unlikely that ill leave it alone
         }
-
-        // try get-ip-intel
-        response = await fetch(`https://www.getipintel.net/check.php?ip=${ip}&flags=m`); // https://www.getipintel.net/ open an issue to change the flags
-        data = await response.text();
-        if (data === '1') {
-            return false // false means unsafe
-        }
-
-        // if it passes both checks;
-        return true // true means safe
-
-    } catch (error) {
-        console.error(`An error has occured ; ${error} ; this user has been excused from the ip blocker`);
-        return true // you could cause an error to occur and get around the block, but its so unlikely that ill leave it alone
-    }
+    });
 }
-
 module.exports = {
-    sleep,
-    getBlockedIps,
-    genHexString,
-    hexToDecimal
+    sleepFunc,
+    getBlockedIpsFunc,
+    genHexStringFunc,
+    hexToDecimalFunc,
+    checkIPAddressFunc
 };
+//# sourceMappingURL=misc.js.map
